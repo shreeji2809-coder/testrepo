046e3e15c0e598ed1a918ad64ea38535ae1b3649
shreejimaharaj <shreejimaharajainfotech2809@gmail.com>
2025-09-28 10:50:18 +0530

    Add files via upload

---
+++ Database/sem_6.sql
@@ -0,0 +1,261 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Feb 26, 2025 at 04:33 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.0.30
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `sem_6`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `permissions`
+--
+
+CREATE TABLE `permissions` (
+  `id` int(11) NOT NULL,
+  `permission` varchar(255) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
+  `createuser` varchar(255) DEFAULT NULL,
+  `deleteuser` varchar(255) DEFAULT NULL,
+  `createbid` varchar(255) DEFAULT NULL,
+  `updatebid` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `permissions`
+--
+
+INSERT INTO `permissions` (`id`, `permission`, `createuser`, `deleteuser`, `createbid`, `updatebid`) VALUES
+(1, 'Superuser', '1', '1', '1', '1'),
+(2, 'Admin', '1', NULL, '1', '1'),
+(3, 'User', NULL, NULL, '1', NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbladmin`
+--
+
+CREATE TABLE `tbladmin` (
+  `ID` int(10) NOT NULL,
+  `UserName` varchar(120) DEFAULT NULL,
+  `FirstName` varchar(255) DEFAULT NULL,
+  `LastName` varchar(255) DEFAULT NULL,
+  `MobileNumber` bigint(10) DEFAULT NULL,
+  `Email` varchar(200) DEFAULT NULL,
+  `Status` int(11) NOT NULL DEFAULT 1,
+  `Password` varchar(120) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `tbladmin`
+--
+
+INSERT INTO `tbladmin` (`ID`, `UserName`, `FirstName`, `LastName`, `MobileNumber`, `Email`, `Status`, `Password`) VALUES
+(1, 'admin', 'meet', 'patel', 9904583634, 'patelmeet12@gmail.com', 1, '71b3b26aaa319e0cdf6fdb8429c112b0');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tblbooking`
+--
+
+CREATE TABLE `tblbooking` (
+  `ID` int(10) NOT NULL,
+  `BookingID` int(10) DEFAULT NULL,
+  `ServiceID` int(10) DEFAULT NULL,
+  `Name` varchar(200) DEFAULT NULL,
+  `MobileNumber` bigint(10) DEFAULT NULL,
+  `Email` varchar(200) DEFAULT NULL,
+  `EventDate` varchar(200) DEFAULT NULL,
+  `EventStartingtime` varchar(200) DEFAULT NULL,
+  `EventEndingtime` varchar(200) DEFAULT NULL,
+  `VenueAddress` mediumtext DEFAULT NULL,
+  `EventType` varchar(200) DEFAULT NULL,
+  `AdditionalInformation` mediumtext DEFAULT NULL,
+  `BookingDate` timestamp NULL DEFAULT current_timestamp(),
+  `Remark` varchar(200) DEFAULT NULL,
+  `Status` varchar(200) DEFAULT NULL,
+  `UpdationDate` timestamp NULL DEFAULT NULL ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `tblbooking`
+--
+
+INSERT INTO `tblbooking` (`ID`, `BookingID`, `ServiceID`, `Name`, `MobileNumber`, `Email`, `EventDate`, `EventStartingtime`, `EventEndingtime`, `VenueAddress`, `EventType`, `AdditionalInformation`, `BookingDate`, `Remark`, `Status`, `UpdationDate`) VALUES
+(1, 342675467, 4, 'Patel Kalpana ', 9978456123, 'patelkalpana12@gmail.com', '2025-02-25', '9 a.m', '11 a.m', 'Decision on call.', 'Engagement ', 'All other information will be on meeting.', '2025-02-26 09:28:38', 'we will soon contact you.', 'Approved', '2025-02-26 09:36:08'),
+(2, 595397966, 3, 'Patel Sai ', 9458671230, 'saipatel13@gmail.com', '20205-04-03', '8 a.m', '3 p.m', 'I tell you in contact', 'Corporate Event', 'More information in meeting ', '2025-02-26 11:07:39', NULL, NULL, NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tblcompany`
+--
+
+CREATE TABLE `tblcompany` (
+  `id` int(11) NOT NULL,
+  `regno` varchar(255) CHARACTER SET latin1 COLLATE latin1_swedish_ci DEFAULT NULL,
+  `companyname` varchar(255) CHARACTER SET latin1 COLLATE latin1_swedish_ci DEFAULT NULL,
+  `companyemail` varchar(255) CHARACTER SET latin1 COLLATE latin1_swedish_ci DEFAULT NULL,
+  `country` varchar(255) CHARACTER SET latin1 COLLATE latin1_swedish_ci DEFAULT NULL,
+  `companyphone` text NOT NULL,
+  `companyaddress` varchar(255) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
+  `companylogo` varchar(255) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL DEFAULT 'avatar15.jpg',
+  `status` varchar(255) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL DEFAULT '0',
+  `creationdate` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbleventtype`
+--
+
+CREATE TABLE `tbleventtype` (
+  `ID` int(10) NOT NULL,
+  `EventType` varchar(200) DEFAULT NULL,
+  `CreationDate` timestamp NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `tbleventtype`
+--
+
+INSERT INTO `tbleventtype` (`ID`, `EventType`, `CreationDate`) VALUES
+(1, 'Wedding', '2025-02-26 09:01:13'),
+(2, 'Private Party', '2025-02-26 09:01:21'),
+(3, 'Corporate Event', '2025-02-26 09:01:29'),
+(4, 'Engagement ', '2025-02-26 09:24:01'),
+(5, 'College Event', '2025-02-26 09:24:42'),
+(6, 'Concert', '2025-02-26 09:24:59'),
+(7, 'Social Event', '2025-02-26 09:25:29');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tblservice`
+--
+
+CREATE TABLE `tblservice` (
+  `ID` int(10) NOT NULL,
+  `ServiceName` varchar(200) DEFAULT NULL,
+  `SerDes` varchar(250) NOT NULL,
+  `ServicePrice` varchar(200) DEFAULT NULL,
+  `CreationDate` timestamp NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `tblservice`
+--
+
+INSERT INTO `tblservice` (`ID`, `ServiceName`, `SerDes`, `ServicePrice`, `CreationDate`) VALUES
+(1, 'Wedding Package', ' All services like  Destination, Catering, Logistics, Music System ,Professional waiters ,	\r\nSpecial Menu and more.', '80000', '2025-02-26 09:07:12'),
+(2, 'Private Party Package', 'All services like Catering ,Entertainment, Music system, Logistics, Photos ,videos, all Rentals things and Professional waiters.', '70000', '2025-02-26 09:10:51'),
+(3, 'Corporate Package', 'All services like Conferences room, Seminars Hall, Dinners ,Professional waiters etc.', '95000', '2025-02-26 09:15:19'),
+(4, 'Engagement Packages', 'All services like Catering, Rentals , Special Menu and Professional waiters etc.', '50000', '2025-02-26 09:19:05'),
+(5, 'Management Package', 'Only Management will be done.\r\n', '40000', '2025-02-26 09:23:02');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `permissions`
+--
+ALTER TABLE `permissions`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `tbladmin`
+--
+ALTER TABLE `tbladmin`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- Indexes for table `tblbooking`
+--
+ALTER TABLE `tblbooking`
+  ADD PRIMARY KEY (`ID`),
+  ADD KEY `ServiceID` (`ServiceID`),
+  ADD KEY `EventType` (`EventType`(191));
+
+--
+-- Indexes for table `tblcompany`
+--
+ALTER TABLE `tblcompany`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `tbleventtype`
+--
+ALTER TABLE `tbleventtype`
+  ADD PRIMARY KEY (`ID`),
+  ADD KEY `EventType` (`EventType`(191));
+
+--
+-- Indexes for table `tblservice`
+--
+ALTER TABLE `tblservice`
+  ADD PRIMARY KEY (`ID`),
+  ADD KEY `ID` (`ID`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `permissions`
+--
+ALTER TABLE `permissions`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `tbladmin`
+--
+ALTER TABLE `tbladmin`
+  MODIFY `ID` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `tblbooking`
+--
+ALTER TABLE `tblbooking`
+  MODIFY `ID` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `tblcompany`
+--
+ALTER TABLE `tblcompany`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `tbleventtype`
+--
+ALTER TABLE `tbleventtype`
+  MODIFY `ID` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT for table `tblservice`
+--
+ALTER TABLE `tblservice`
+  MODIFY `ID` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
